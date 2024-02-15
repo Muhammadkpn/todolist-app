d9adabd685ad4fde6bb5370f2acee970a8bb51c0
hansvintsugata <hansvintsugata@gmail.com>
2024-02-16 00:00:55 +0700

    change sdklog filePath

--- pkg/resource/injection/sdklog.go
+++ pkg/resource/injection/sdklog.go
@@ -7,6 +7,7 @@ import (
 func InitSdkLog() sdklog.Logger {
 	// init logger
 	sdklogCfg := sdklog.Config{
+		LogPath:          "./logs",
 		EnableConsoleLog: true,
 		Level:            sdklog.DebugLevel,
 	}
